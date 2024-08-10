e741cb95e5b7cfcb028c4ba3463a4d01a6257d8f
Quentin D <d.quentin.128@gmail.com>
2024-08-10 20:51:05 +0200

    Adding sql script to fill database

---
+++ app/sql/data.sql
@@ -0,0 +1,135 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    surname VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Hart', 'Kiona'),
+  ('Glover', 'Kieran'),
+  ('Dillard', 'Reuben'),
+  ('Hays', 'Tatyana'),
+  ('Randall', 'Driscoll'),
+  ('Franks', 'Jade'),
+  ('Jenkins', 'Carol'),
+  ('Stevenson', 'Genevieve'),
+  ('Park', 'Samson'),
+  ('Maldonado', 'Whoopi');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Mayer', 'Axel'),
+  ('Skinner', 'Katelyn'),
+  ('Doyle', 'Odessa'),
+  ('Frank', 'Inez'),
+  ('Gonzales', 'Arthur'),
+  ('Garza', 'Melyssa'),
+  ('Zamora', 'Veronica'),
+  ('Shelton', 'Illana'),
+  ('Mcmillan', 'Declan'),
+  ('Rasmussen', 'Byron');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Steele', 'Gwendolyn'),
+  ('Savage', 'Tanya'),
+  ('Jones', 'Allen'),
+  ('Pitts', 'Idona'),
+  ('Weber', 'Carolyn'),
+  ('Spencer', 'Brenna'),
+  ('Battle', 'Igor'),
+  ('Holloway', 'Macy'),
+  ('Camacho', 'Yuri'),
+  ('Luna', 'Tatum');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Zimmerman', 'Jakeem'),
+  ('Brooks', 'Valentine'),
+  ('Petersen', 'Kenyon'),
+  ('Jackson', 'Maggy'),
+  ('Pittman', 'Lucius'),
+  ('Hurley', 'Shea'),
+  ('Pittman', 'Jena'),
+  ('Cantrell', 'Brian'),
+  ('Guzman', 'Shana'),
+  ('Garza', 'Jerome');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Cherry', 'Zia'),
+  ('Woodard', 'Cara'),
+  ('Goodwin', 'Aladdin'),
+  ('Pace', 'Stella'),
+  ('Morin', 'Shea'),
+  ('Cox', 'Kennedy'),
+  ('Tucker', 'Quentin'),
+  ('Snider', 'Austin'),
+  ('Miles', 'Francesca'),
+  ('Bryan', 'May');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Holden', 'Sara'),
+  ('Sanders', 'Shannon'),
+  ('Prince', 'Geoffrey'),
+  ('Glenn', 'Illana'),
+  ('Delaney', 'Erin'),
+  ('French', 'Kylie'),
+  ('O''brien', 'Micah'),
+  ('Salas', 'Benjamin'),
+  ('Arnold', 'Laurel'),
+  ('Sanford', 'Katelyn');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Roberson', 'Lucius'),
+  ('Thompson', 'Ira'),
+  ('Craft', 'Nayda'),
+  ('Mckinney', 'Kiayada'),
+  ('Thomas', 'Lynn'),
+  ('Christian', 'Aline'),
+  ('Cain', 'Brendan'),
+  ('Hansen', 'Walker'),
+  ('Barnes', 'Derek'),
+  ('Conrad', 'Meghan');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Hamilton', 'Ann'),
+  ('Wiggins', 'Isaiah'),
+  ('Mccarthy', 'Audrey'),
+  ('Cameron', 'Brandon'),
+  ('Munoz', 'Teagan'),
+  ('Bush', 'Mikayla'),
+  ('Foley', 'Sebastian'),
+  ('Holloway', 'Yardley'),
+  ('Boone', 'Holly'),
+  ('Hood', 'Patrick');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Finch', 'Tate'),
+  ('Thompson', 'Shellie'),
+  ('Green', 'Cameran'),
+  ('Steele', 'Keith'),
+  ('Watts', 'Hamilton'),
+  ('Carver', 'Armand'),
+  ('Hooper', 'April'),
+  ('Morales', 'Alexis'),
+  ('Page', 'Kuame'),
+  ('Jimenez', 'Francis');
+
+INSERT INTO users (surname, name)
+VALUES
+  ('Sweeney', 'Gray'),
+  ('Beach', 'Hammett'),
+  ('Parsons', 'Nola'),
+  ('Kaufman', 'Charles'),
+  ('Howe', 'Palmer'),
+  ('Howe', 'Omar'),
+  ('Holder', 'Isabelle'),
+  ('Fisher', 'Kane'),
+  ('Munoz', 'Kevyn'),
+  ('Barker', 'Fay');
